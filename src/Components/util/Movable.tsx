d0e8a0153255453a2f493a3b61288ccf31d07ae2
enzodfer <enzodfer@outlook.com>
2023-01-26 20:24:25 -0500

    Add documentation to Movable

--- src/Components/util/Movable.tsx
+++ src/Components/util/Movable.tsx
@@ -1,20 +1,40 @@
 import React, { useRef, useState } from 'react'
 
+/**
+ * TPoint2D type describes x and y coordinates
+ */
 type TPoint2D = {
   x: number,
   y: number
 }
 
+/**
+ * TMovableProps describes the props that the Movable component takes
+ *
+ * @prop children - Children components that the Movable component wraps
+ */
 type TMovableProps = {
   children: React.ReactNode
 }
 
+/**
+
+  Movable is a functional component that allows for draggable functionality of its children element by either mouse or touch events.
+  @param {TMovableProps} props - The props for the component, it requires a children prop to be passed.
+  @returns {JSX.Element} - Returns a JSX div element that has the draggable functionality implemented
+*/
 export const Movable = ( {children}:TMovableProps ): JSX.Element => {
   const [coord,setCoord] = useState<TPoint2D>();
   const [offset, setOffset] = useState<TPoint2D>();
   const iconRef: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
   const [isDown, setIsDown] = useState<boolean>(false);
 
+  /**
+   * handleDown is a function that sets the initial position of the children component
+   * when the user starts moving it using mouse or touch events.
+   *
+   * @param e - MouseEvent or TouchEvent
+   */
   function handleDown(e: React.TouchEvent<HTMLDivElement> | React.MouseEvent<HTMLDivElement, MouseEvent>) {
     if (!iconRef.current || !iconRef) {
       throw new Error('No reference found to icon for mobile touch')
@@ -35,6 +55,12 @@ export const Movable = ( {children}:TMovableProps ): JSX.Element => {
     }
   }
 
+  /**
+   * handleMove is a function that updates the position of the children component
+   * as the user moves it using mouse or touch events.
+   *
+   * @param e - MouseEvent or TouchEvent
+   */
   function handleMove(e: React.TouchEvent<HTMLDivElement> | React.MouseEvent<HTMLDivElement, MouseEvent>) {
     if (!isDown || !offset) return;
 
@@ -52,8 +78,15 @@ export const Movable = ( {children}:TMovableProps ): JSX.Element => {
     }
   }
 
+  /**
+   * handleStop is a function that stops the movement of the children component
+   * when the user releases the mouse button or touch event.
+   *
+   * @param e - MouseEvent or TouchEvent
+   */
   function handleStop(e: React.TouchEvent<HTMLDivElement> | React.MouseEvent<HTMLDivElement, MouseEvent>) {
     setIsDown(false);  
+    // Reset the offset values when the user stops moving the element
     setOffset({
       x: 0,
       y: 0,
